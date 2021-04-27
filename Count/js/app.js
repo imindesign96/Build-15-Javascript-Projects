eb99dd6b250fb8fa9846e0cd4a407bafd4e3a565
imindesign96 <phanngocvu89@gmail.com>
2021-04-27 21:57:39 +0900

    final

--- Count/js/app.js
+++ Count/js/app.js
@@ -16,7 +16,16 @@ btns.forEach(function(btn){
            count++;
        }else
             count = 0;
-       value.textContent = count;
+        if(count > 0){
+            value.style.color = "green";
+        }
+        if(count < 0){
+            value.style.color = "red";
+        }
+        if(count === 0 ){
+            value.style.color = "black";
+        }
+        value.textContent = count;
     })
     
 });
